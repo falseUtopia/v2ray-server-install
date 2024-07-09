c9fa7aa870d96879c0a1cff92258365b40491da1
falseu <falseutopia@163.com>
2024-07-10 01:30:19 +0800

    调整构建脚本

--- release.bat
+++ release.bat
@@ -6,5 +6,6 @@ set current_dir=%CD%
 cd src\main\java\com\falseu\autoinstallproxy
 javac.exe -cp "hutool-all-5.8.28.jar;." -encoding UTF-8 -d . AutoInstallProxy.java
 java -cp "hutool-all-5.8.28.jar;." com.falseu.autoinstallproxy.AutoInstallProxy release %current_dir%
+cd ../../../../../../
 echo 按任意键退出
 pause>nul
