5c64e720d0548d9562371e4da47f0d977d543bf3
falseu <falseutopia@163.com>
2024-07-10 01:54:59 +0800

    清理安装文件

--- src/main/java/com/falseu/autoinstallproxy/start.bat
+++ src/main/java/com/falseu/autoinstallproxy/start.bat
@@ -4,5 +4,7 @@ echo 处理中...
 set script_dir=%~dp0
 
 java -cp "%script_dir%hutool-all-5.8.28.jar;%script_dir%;." com.falseu.autoinstallproxy.AutoInstallProxy %1
+del "%script_dir%..\v2ray-proxy-install.zip"
+rd /s /q "%script_dir%..\v2ray-proxy-install"
 echo 没错误就表示完成,按任意键退出
 pause>nul
